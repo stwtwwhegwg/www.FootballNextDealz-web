92f1da9974737b55b2b57a7e5b2194f607a07de0
stwtwwhegwg <abdelashalaby7@gmail.com>
2025-06-01 19:59:16 +0200

    Create src-App.js

---
+++ src-App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '50px'}}>
+      <h1>Willkommen bei FootballNextDealz!</h1>
+      <p>Deine neue Fußball-Webseite ist bereit.</p>
+    </div>
+  );
+}
+
+export default App;
